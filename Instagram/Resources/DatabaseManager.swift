5d26bf182631074ec87e9c440addb762587f8dbd
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-02-23 23:24:50 +0800

    create database manager

---
+++ Instagram/Resources/DatabaseManager.swift
@@ -0,0 +1,16 @@
+//
+//  DatabaseManager.swift
+//  Instagram
+//
+//  Created by Marko Luther Antiola on 2/23/22.
+//
+
+import FirebaseDatabase
+
+public class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+   
+    
+}
