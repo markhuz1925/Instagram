a309e4db60354fedcdabb6003a3d86a761eb1e60
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-02-23 23:23:40 +0800

    creating views

--- Instagram/Controllers/Others/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Others/Onboarding/LoginViewController.swift
@@ -8,10 +8,60 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let userNameEmailTextField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton ()
+    }()
+    
+    private let registerButton: UIButton = {
+        return UIButton ()
+    }()
+    
+    private let termsConditionButton: UIButton = {
+        return UIButton ()
+    }()
+
+    private let privacyPolicyButton: UIButton = {
+        return UIButton ()
+    }()
+    
+    private let headerView: UIView = {
+        return UIView()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubViews()
+        view.backgroundColor = .systemBackground
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    private func addSubViews() {
+        view.addSubview(userNameEmailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(registerButton)
+        view.addSubview(termsConditionButton)
+        view.addSubview(privacyPolicyButton)
+        view.addSubview(headerView)
+    }
+    
+    @objc private func didTapLogin() {}
+    @objc private func didTaptermsConditionButton() {}
+    @objc private func didTapprivacyPolicyButton() {}
+    @objc private func didTapRegister() {}
+    
 }
