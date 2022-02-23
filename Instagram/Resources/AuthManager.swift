5eca5d90b5b0afb21321cc267c3edaa424adc913
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-02-23 23:24:40 +0800

    create auth manager

---
+++ Instagram/Resources/AuthManager.swift
@@ -0,0 +1,22 @@
+//
+//  AuthManager.swift
+//  Instagram
+//
+//  Created by Marko Luther Antiola on 2/23/22.
+//
+
+import FirebaseAuth
+
+public class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    public func registerNewUser(userName: String, email: String, password: String) {
+        
+    }
+    
+    public func loginUser(userName: String?, email: String?, password: String) {
+        
+    }
+    
+}
