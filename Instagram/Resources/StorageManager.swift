c938b268e2a5fa18e30b513b89e6f6a0517acb51
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-02-23 23:24:59 +0800

    create storage manager

---
+++ Instagram/Resources/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Marko Luther Antiola on 2/23/22.
+//
+
+import FirebaseStorage
+
+public class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    
+}
